03203979829325f4c77679e1d97bef830e90a718
jpdevicente <jpdevicente@uc.cl>
2019-04-29 14:39:29 -0400

    sdfsdfsdf

---
+++ ejemplo.py
@@ -0,0 +1,27 @@
+from threading import Thread, Lock
+from time import sleep
+
+mi_lock = Lock()
+lock_2 = Lock()
+
+def contador_a():
+	while True:
+		with mi_lock:
+			print(-1)
+		with lock_2:
+			sleep(1)
+		
+			
+					
+def contador_b():
+	while True:
+		with mi_lock:
+			print(" jamon")
+		with lock_2:
+			sleep(2)
+
+t1 = Thread(target=contador_a)
+t2 = Thread(target=contador_b)
+
+t1.start()
+t2.start()
